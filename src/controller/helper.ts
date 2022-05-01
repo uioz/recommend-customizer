81a56bee9d98a9f97b0b7ea5d1bbea4bbb6deea4
ASCll <hzxbpld@163.com>
2022-05-01 23:35:59 +0800

    fix: code match improvement

--- src/controller/helper.ts
+++ src/controller/helper.ts
@@ -12,13 +12,13 @@ export const matchWraper: matchAble = (regexp, defaultStr) => (str) => {
 };
 
 // match code like 000xxx-000
-export const numberPrefixCode = matchWraper(/^\d{3}([a-z]{2,5})-\d{3,5}$/i);
+export const numberPrefixCode = matchWraper(/^\d{3}([a-z]{2,6})-\d{3,5}$/i);
 // match code liek fc2-ppv-1234567
 export const fc2PrefixCode = matchWraper(/^(fc2)-(?:ppv-|)\d{7}$/i);
 // match code like t-28000
 export const t28PrefixCode = matchWraper(/^(?:t-28\d{3})|t28-\d{3}$/i, "t28");
 // match code like
-export const normalCode = matchWraper(/^([a-z]{2,5})-\d{3,5}$/i);
+export const normalCode = matchWraper(/^([a-z]{2,6})-\d{3,5}$/i);
 
 export const codePatterns = [
   numberPrefixCode,
