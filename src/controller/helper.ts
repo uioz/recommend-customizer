b9d98c2e0ebf97bcc153d9ccb2bde1451e7854df
ASCll <hzxbpld@163.com>
2022-05-02 21:28:17 +0800

    fix: more powerful regexp for code matching

--- src/controller/helper.ts
+++ src/controller/helper.ts
@@ -11,21 +11,19 @@ export const matchWraper: matchAble = (regexp, defaultStr) => (str) => {
   return result;
 };
 
-// match code like 000xxx-000
-export const numberPrefixCode = matchWraper(/^\d{3}([a-z]{2,6})-\d{3,5}$/i);
 // match code liek fc2-ppv-1234567
 export const fc2PrefixCode = matchWraper(/^(fc2)-(?:ppv-|)\d{7}$/i);
 // match code like t-28000
 export const t28PrefixCode = matchWraper(/^(?:t-28\d{3})|t28-\d{3}$/i, "t28");
 // match code like
-export const normalCode = matchWraper(/^([a-z]{2,6})-\d{3,5}$/i);
+// 000xxx-000
+// XXX-000Z
+// 300XXX-000
+export const numberPrefixCode = matchWraper(
+  /^((?:\d{3})?[a-z]{2,7}-\d{2,5}(?:[a-z])?)$/i
+);
 
-export const codePatterns = [
-  numberPrefixCode,
-  fc2PrefixCode,
-  t28PrefixCode,
-  normalCode,
-];
+export const codePatterns = [numberPrefixCode, fc2PrefixCode, t28PrefixCode];
 
 export function getCodePrefix(fullCode: string): string | null {
   for (const pattern of codePatterns) {
