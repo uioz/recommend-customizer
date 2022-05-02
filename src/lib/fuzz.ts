806a0b1229f9b48a3951aab26a19df8c7facb6c6
ASCll <hzxbpld@163.com>
2022-05-02 15:34:56 +0800

    fix: improve accuracy of fuzz

--- src/lib/fuzz.ts
+++ src/lib/fuzz.ts
@@ -4,12 +4,20 @@ import Fuse from "fuse.js";
 const LOG_TARGET = "fuzz";
 
 const searchEngine = new Fuse<string>([], {
-  threshold: 0.93,
+  threshold: 0.94,
+  minMatchCharLength: 3,
 });
 
 export async function init() {
   console.log(`${LOG_TARGET} init`);
   searchEngine.setCollection(await ActressStore.read());
+
+  ActressStore.event.on((actress) => {
+    for (const item of actress) {
+      searchEngine.add(item);
+    }
+  });
+
   console.log(`${LOG_TARGET} success`);
 }
 
