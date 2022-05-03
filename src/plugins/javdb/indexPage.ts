51628d37754553ac8281ff355d75881476467520
ASCll <hzxbpld@163.com>
2022-05-03 16:21:30 +0800

    feat: 优化页面浏览体验

--- src/plugins/javdb/indexPage.ts
+++ src/plugins/javdb/indexPage.ts
@@ -9,15 +9,13 @@ export function script(meta: unknown, detailPathReg: string) {
 
   function extract(elem: HTMLAnchorElement): CodeRequest | undefined {
     try {
-      const href = elem.getAttribute("href") as string;
+      const key = elem.getAttribute("href") as string;
       const title = elem.getAttribute("title")?.trim() as string;
       const code = elem.querySelector("strong")?.innerText.trim() as string;
       const rank = parseFloat(
         // @ts-ignore
         elem.querySelector(".value")?.innerText.trim().substring(0, 3)
       );
-      // @ts-ignore
-      const [_, key] = matchKey.exec(href);
 
       return {
         // @ts-ignore
@@ -31,22 +29,60 @@ export function script(meta: unknown, detailPathReg: string) {
     }
   }
 
+  const keyToRank = new Map<string, number>();
+
   const data = Array.from<HTMLAnchorElement>(
     document.querySelectorAll(".box")
-  ).map(extract);
+  ).map((item) => {
+    const result = extract(item);
+    if (result) {
+      keyToRank.set(result.key, result?.rank ?? 0);
+    }
+    return result;
+  });
 
   function sorting(sortedMap: Array<{ key: string; weight: number }>) {
     const container = document.querySelector(".movie-list") as HTMLElement;
-    const elems = sortedMap.map(({ key, weight }) => {
-      const item = container.querySelector(`[href="/v/${key}"]`) as HTMLElement;
+    const freshItems: Array<HTMLElement> = [];
+    const lowRankItems: Array<HTMLElement> = [];
+
+    for (const { key, weight } of sortedMap) {
+      const anchor = container.querySelector(`[href="${key}"]`) as HTMLElement;
       const span = document.createElement("span");
       span.className = "tag is-success";
-      span.innerText = weight + "";
-      item.querySelector(".tags.has-addons")?.appendChild(span);
-      return item.parentElement as HTMLElement;
-    });
+      span.innerText = weight.toFixed(2);
+      anchor.querySelector(".tags.has-addons")?.appendChild(span);
 
-    container.replaceChildren(...elems);
+      const rank = keyToRank.get(key);
+
+      if (rank !== undefined && rank < 3.5) {
+        lowRankItems.push(anchor.parentElement as HTMLElement);
+        continue;
+      }
+
+      freshItems.push(anchor.parentElement as HTMLElement);
+    }
+
+    const lowRankHead = document.createElement("h1");
+    lowRankHead.innerText = "低分";
+
+    const lowRankContainer = container.cloneNode(false) as HTMLElement;
+    lowRankContainer.append(...lowRankItems);
+
+    const toolbar = document.querySelector(".index-toolbar") as HTMLElement;
+    const pagination = document.querySelector(".pagination") as HTMLElement;
+    pagination.style.position = "sticky";
+    pagination.style.top = "65px";
+    pagination.style.borderRadius = "1em";
+    pagination.style.zIndex = "100";
+    pagination.style.background = "#ffffffd9";
+    pagination.style.padding = "1em";
+    // @ts-ignore
+    pagination.style.backdropFilter = "blur(7px)";
+
+    toolbar.after(pagination);
+    container.replaceChildren(...freshItems);
+    container.after(lowRankHead, lowRankContainer);
   }
 
   let gotResponse = false;
